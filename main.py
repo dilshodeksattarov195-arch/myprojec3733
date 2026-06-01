2e531eb3fafbec64ff7493313f0c61d62399372f
DILSHODBEK <dilshodsattar26@gmail.com>
2026-06-01 06:59:42 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class productPonnectWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = productPonnectWorker()
+    print("Worker engine initialized.")
